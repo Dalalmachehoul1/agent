fb74f10fc6820c71003ff60341730aaf862a4c6b
oumaima324 <oumaima.mechehoul@gmail.com>
2025-09-18 08:43:41 +0200

    add HR tools blueprint and example route

--- app/blueprints/hr_tools/routes.py
+++ app/blueprints/hr_tools/routes.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, jsonify
+
+hr_tools_bp = Blueprint("hr_tools", __name__)
+
+@hr_tools_bp.route("/tool1", methods=["GET"])
+def tool1():
+    return jsonify({"message": "Exemple outil RH 1"})
 
